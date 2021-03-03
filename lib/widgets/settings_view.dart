502b20c51ddc0760654993498b88a1a242d18171
onur-yildiz <onur.yildizcp13@hotmail.com>
2021-03-04 01:34:29 +0300

    added social links and app name on drawer

--- lib/widgets/settings_view.dart
+++ lib/widgets/settings_view.dart
@@ -3,7 +3,9 @@ import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_quit_addiction_app/extensions/string_extension.dart';
 import 'package:flutter_quit_addiction_app/providers/settings_provider.dart';
 import 'package:flutter_quit_addiction_app/widgets/currency_picker.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:provider/provider.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class SettingsView extends StatefulWidget {
   @override
@@ -21,88 +23,265 @@ class _SettingsViewState extends State<SettingsView> {
     final deviceHeight = mediaQuery.size.height;
     // final deviceWidth = mediaQuery.size.width;
     return Drawer(
-      child: ListView(
-        children: [
-          SizedBox(
-            height: deviceHeight * .1,
-            child: DrawerHeader(
-              decoration: BoxDecoration(
-                border: Border(
-                  bottom: BorderSide(
-                    width: 1,
-                    color: Theme.of(context).primaryColorDark,
-                  ),
-                ),
-              ),
-              child: Text(
-                local.settings.capitalizeWords(),
-                style: TextStyle(
-                  fontSize: Theme.of(context).textTheme.headline6.fontSize,
-                  fontWeight: FontWeight.bold,
-                  color: Theme.of(context).primaryColor,
+        child: LayoutBuilder(
+      builder: (context, constraints) => SingleChildScrollView(
+        child: Container(
+          child: ConstrainedBox(
+            constraints: constraints.copyWith(
+              minHeight: constraints.maxHeight,
+              maxHeight: double.infinity,
+            ),
+            child: IntrinsicHeight(
+              child: SafeArea(
+                child: Column(
+                  children: [
+                    Column(
+                      children: [
+                        Container(
+                          height: kToolbarHeight,
+                          width: double.infinity,
+                          child: DrawerHeader(
+                            decoration: BoxDecoration(
+                              border: Border(
+                                bottom: BorderSide(
+                                  width: 1,
+                                  color: Theme.of(context).primaryColor,
+                                ),
+                              ),
+                            ),
+                            child: Text(
+                              local.settings.capitalizeWords(),
+                              style: TextStyle(
+                                fontSize: Theme.of(context)
+                                    .textTheme
+                                    .headline6
+                                    .fontSize,
+                                fontWeight: FontWeight.bold,
+                                color: Theme.of(context).primaryColor,
+                              ),
+                            ),
+                          ),
+                        ),
+                        CheckboxListTile(
+                          contentPadding: const EdgeInsets.symmetric(
+                              vertical: 24, horizontal: 8),
+                          title: Text(
+                              local.progressNotification.capitalizeWords()),
+                          subtitle: Text(local.progressNotificationDesc
+                              .capitalizeFirstLetter()),
+                          value: _progressCheck,
+                          onChanged: (value) {
+                            if (mounted) {
+                              setState(() {
+                                _progressCheck = value;
+                              });
+                            }
+                          },
+                        ),
+                        Divider(
+                          height: 0,
+                          thickness: 1,
+                        ),
+                        CheckboxListTile(
+                          contentPadding: const EdgeInsets.symmetric(
+                              vertical: 24, horizontal: 8),
+                          title: Text(local.quoteOfTheDay.capitalizeWords()),
+                          subtitle: Text(
+                              local.quoteOfTheDayDesc.capitalizeFirstLetter()),
+                          value: _quoteCheck,
+                          onChanged: (value) {
+                            if (mounted) {
+                              setState(() {
+                                _quoteCheck = value;
+                              });
+                            }
+                          },
+                        ),
+                        Divider(
+                          height: 0,
+                          thickness: 1,
+                        ),
+                        ListTile(
+                          dense: true,
+                          onTap: () => showDialog(
+                            context: context,
+                            builder: (context) => CurrencyPicker(),
+                          ),
+                          contentPadding: const EdgeInsets.symmetric(
+                              vertical: 8, horizontal: 8),
+                          leading: Icon(Icons.account_balance_wallet_outlined),
+                          title: Text(local.currency.capitalizeWords()),
+                          subtitle: Text(
+                              Provider.of<SettingsProvider>(context).currency),
+                        ),
+                        Divider(
+                          height: 0,
+                          thickness: 1,
+                        ),
+                      ],
+                    ),
+                    Expanded(
+                      child: Align(
+                        alignment: Alignment.bottomCenter,
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text(
+                                local.appName.toUpperCase(),
+                                style: TextStyle(
+                                  color: Theme.of(context).primaryColor,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              Row(
+                                children: [
+                                  IconButton(
+                                    icon: FaIcon(
+                                      FontAwesomeIcons.twitter,
+                                      color: Color.fromRGBO(29, 161, 242, 1),
+                                    ),
+                                    tooltip: 'Twitter',
+                                    onPressed: () {
+                                      _launchURL(
+                                          'https://twitter.com/onur_ldz');
+                                    },
+                                  ),
+                                  IconButton(
+                                    icon: FaIcon(
+                                      FontAwesomeIcons.github,
+                                    ),
+                                    tooltip: 'GitHub',
+                                    onPressed: () {
+                                      _launchURL(
+                                          'https://github.com/onur-yildiz');
+                                    },
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    )
+                  ],
                 ),
               ),
             ),
           ),
-          Wrap(
-            children: [
-              CheckboxListTile(
-                contentPadding:
-                    const EdgeInsets.symmetric(vertical: 24, horizontal: 8),
-                title: Text(local.progressNotification.capitalizeWords()),
-                subtitle: Text(
-                    local.progressNotificationDesc.capitalizeFirstLetter()),
-                value: _progressCheck,
-                onChanged: (value) {
-                  if (mounted) {
-                    setState(() {
-                      _progressCheck = value;
-                    });
-                  }
-                },
-              ),
-              Divider(
-                height: 0,
-                thickness: 1,
-              ),
-              CheckboxListTile(
-                contentPadding:
-                    const EdgeInsets.symmetric(vertical: 24, horizontal: 8),
-                title: Text(local.quoteOfTheDay.capitalizeWords()),
-                subtitle: Text(local.quoteOfTheDayDesc.capitalizeFirstLetter()),
-                value: _quoteCheck,
-                onChanged: (value) {
-                  if (mounted) {
-                    setState(() {
-                      _quoteCheck = value;
-                    });
-                  }
-                },
-              ),
-              Divider(
-                height: 0,
-                thickness: 1,
-              ),
-              ListTile(
-                dense: true,
-                onTap: () => showDialog(
-                  context: context,
-                  builder: (context) => CurrencyPicker(),
-                ),
-                contentPadding:
-                    const EdgeInsets.symmetric(vertical: 8, horizontal: 8),
-                leading: Icon(Icons.account_balance_wallet_outlined),
-                title: Text(local.currency.capitalizeWords()),
-                subtitle: Text(Provider.of<SettingsProvider>(context).currency),
-              ),
-              Divider(
-                height: 0,
-                thickness: 1,
-              ),
-            ],
-          ),
-        ],
+        ),
       ),
-    );
+    )
+        // ListView(
+        //   children: [
+        //     SizedBox(
+        //       height: deviceHeight * .1,
+        //       child: DrawerHeader(
+        //         decoration: BoxDecoration(
+        //           border: Border(
+        //             bottom: BorderSide(
+        //               width: 1,
+        //               color: Theme.of(context).primaryColorDark,
+        //             ),
+        //           ),
+        //         ),
+        //         child: Text(
+        //           local.settings.capitalizeWords(),
+        //           style: TextStyle(
+        //             fontSize: Theme.of(context).textTheme.headline6.fontSize,
+        //             fontWeight: FontWeight.bold,
+        //             color: Theme.of(context).primaryColor,
+        //           ),
+        //         ),
+        //       ),
+        //     ),
+        //     Wrap(
+        //       children: [
+        //         CheckboxListTile(
+        //           contentPadding:
+        //               const EdgeInsets.symmetric(vertical: 24, horizontal: 8),
+        //           title: Text(local.progressNotification.capitalizeWords()),
+        //           subtitle: Text(
+        //               local.progressNotificationDesc.capitalizeFirstLetter()),
+        //           value: _progressCheck,
+        //           onChanged: (value) {
+        //             if (mounted) {
+        //               setState(() {
+        //                 _progressCheck = value;
+        //               });
+        //             }
+        //           },
+        //         ),
+        //         Divider(
+        //           height: 0,
+        //           thickness: 1,
+        //         ),
+        //         CheckboxListTile(
+        //           contentPadding:
+        //               const EdgeInsets.symmetric(vertical: 24, horizontal: 8),
+        //           title: Text(local.quoteOfTheDay.capitalizeWords()),
+        //           subtitle: Text(local.quoteOfTheDayDesc.capitalizeFirstLetter()),
+        //           value: _quoteCheck,
+        //           onChanged: (value) {
+        //             if (mounted) {
+        //               setState(() {
+        //                 _quoteCheck = value;
+        //               });
+        //             }
+        //           },
+        //         ),
+        //         Divider(
+        //           height: 0,
+        //           thickness: 1,
+        //         ),
+        //         ListTile(
+        //           dense: true,
+        //           onTap: () => showDialog(
+        //             context: context,
+        //             builder: (context) => CurrencyPicker(),
+        //           ),
+        //           contentPadding:
+        //               const EdgeInsets.symmetric(vertical: 8, horizontal: 8),
+        //           leading: Icon(Icons.account_balance_wallet_outlined),
+        //           title: Text(local.currency.capitalizeWords()),
+        //           subtitle: Text(Provider.of<SettingsProvider>(context).currency),
+        //         ),
+        //         Divider(
+        //           height: 0,
+        //           thickness: 1,
+        //         ),
+        //         Row(
+        //           children: [
+        //             Text(
+        //               local.appName.toUpperCase(),
+        //               style: TextStyle(
+        //                 color: Theme.of(context).primaryColor,
+        //                 fontWeight: FontWeight.bold,
+        //               ),
+        //             ),
+        //             IconButton(
+        //               icon: FaIcon(FontAwesomeIcons.twitter),
+        //               onPressed: () {
+        //                 _launchURL('https://twitter.com/onur_ldz');
+        //               },
+        //             ),
+        //             IconButton(
+        //               icon: FaIcon(FontAwesomeIcons.github),
+        //               onPressed: () {
+        //                 _launchURL('https://github.com/onur-yildiz');
+        //               },
+        //             ),
+        //           ],
+        //         ),
+        //       ],
+        //     ),
+        //   ],
+        // ),
+
+        );
   }
 }
+
+void _launchURL(url) async =>
+    await canLaunch(url) ? await launch(url) : throw 'Could not launch $url';
