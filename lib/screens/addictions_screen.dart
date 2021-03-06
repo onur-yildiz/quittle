b59b1b461ff0a9a9e5e00aed55b87c6c8f41174a
onur-yildiz <onur.yildizcp13@hotmail.com>
2021-03-07 01:26:49 +0300

    removed SideMenu, removed bottomtabbar, added appbar, changed ListView to ReorderableListView

--- lib/screens/addictions_screen.dart
+++ lib/screens/addictions_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:quittle/extensions/string_extension.dart';
+import 'package:quittle/helpers/db_helper.dart';
 import 'package:quittle/models/addiction.dart';
 import 'package:quittle/providers/addictions_provider.dart';
 import 'package:quittle/providers/settings_provider.dart';
@@ -9,7 +10,7 @@ import 'package:quittle/util/progress_constants.dart';
 import 'package:quittle/widgets/addiction_item_card.dart';
 import 'package:quittle/widgets/settings_view.dart';
 import 'package:provider/provider.dart';
-import 'package:shrink_sidemenu/shrink_sidemenu.dart';
+import 'package:uuid/uuid.dart';
 import 'package:workmanager/workmanager.dart';
 
 class AddictionsScreen extends StatefulWidget {
@@ -21,7 +22,6 @@ class AddictionsScreen extends StatefulWidget {
 
 class _AddictionsScreenState extends State<AddictionsScreen> {
   final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey();
-  final GlobalKey<SideMenuState> _sideMenuKey = GlobalKey<SideMenuState>();
 
   _setProgNotifTasks(List<Addiction> addictions) {
     if (Provider.of<SettingsProvider>(context, listen: false)
@@ -37,6 +37,13 @@ class _AddictionsScreenState extends State<AddictionsScreen> {
             },
             frequency: Duration(minutes: 15),
             existingWorkPolicy: ExistingWorkPolicy.keep,
+            constraints: Constraints(
+              requiresDeviceIdle: false,
+              requiresBatteryNotLow: false,
+              requiresCharging: false,
+              requiresStorageNotLow: false,
+              networkType: NetworkType.not_required,
+            ),
           );
         } else {
           Workmanager.cancelByUniqueName(addiction.id);
@@ -59,140 +66,132 @@ class _AddictionsScreenState extends State<AddictionsScreen> {
       );
     }
 
-    return WillPopScope(
-      onWillPop: () async {
-        final _state = _sideMenuKey.currentState;
-        if (_state.isOpened) {
-          _state.closeSideMenu();
-          return false;
-        }
-        return true;
-      },
-      child: SideMenu(
-        key: _sideMenuKey,
-        menu: SettingsView(),
-        type: SideMenuType.slideNRotate,
-        background: Theme.of(context).primaryColor,
-        maxMenuWidth: deviceSize.width * .8,
-        child: Scaffold(
-          key: _scaffoldKey,
-          appBar: AppBar(
-              leading: IconButton(
-            icon: Icon(Icons.menu),
-            onPressed: () {
-              final _state = _sideMenuKey.currentState;
-              if (_state.isOpened)
-                _state.closeSideMenu();
-              else
-                _state.openSideMenu();
-            },
-          )),
-          floatingActionButton: Consumer<AddictionsProvider>(
-            child: FloatingActionButton(
-              heroTag: 'newAddiction',
-              onPressed: pushCreateAddictionScreen,
-              backgroundColor: Theme.of(context).primaryColor,
-              tooltip: 'New',
-              child: Icon(Icons.add),
-            ),
-            builder: (_, addictionsData, child) {
-              return addictionsData.addictions.length == 0
-                  ? SizedBox()
-                  : SizedBox(
-                      child: child,
-                    );
-            },
-          ),
-          floatingActionButtonLocation:
-              FloatingActionButtonLocation.centerFloat,
-          // bottomNavigationBar: Builder(
-          //   builder: (context) => BottomAppBar(
-          //     child: Row(
-          //       mainAxisAlignment: MainAxisAlignment.start,
-          //       children: [
-          //         IconButton(
-          //           onPressed: () {
-          //             // Scaffold.of(context).openDrawer();
-          //             final _state = _sideMenuKey.currentState;
-          //             if (_state.isOpened)
-          //               _state.closeSideMenu(); // close side menu
-          //             else
-          //               _state.openSideMenu(); // open side menu
-          //           },
-          //           color: Theme.of(context).primaryColor,
-          //           tooltip: 'Menu',
-          //           icon: Icon(Icons.menu),
-          //         ),
-          //       ],
-          //     ),
-          //   ),
-          // ),
-          drawer: SettingsView(),
-          body: Consumer<AddictionsProvider>(
-            builder: (ctx, addictionsData, child) {
-              _setProgNotifTasks(addictionsData.addictions);
-              return addictionsData.addictions.length > 0
-                  ? ListView.builder(
-                      itemCount: addictionsData.addictions.length,
-                      itemBuilder: (ctx, index) {
-                        return AddictionItem(
-                          addictionData: addictionsData.addictions[index],
-                        );
-                      },
-                    )
-                  : InkWell(
-                      onTap: pushCreateAddictionScreen,
-                      child: Container(
-                        height: deviceSize.height,
-                        width: deviceSize.width,
-                        child: Stack(
-                          alignment: Alignment.center,
-                          children: [
-                            Center(
-                              child: SizedBox.fromSize(
-                                size: Size.square(deviceSize.width * .5),
-                                child: FloatingActionButton(
-                                  heroTag: 'newAddiction',
-                                  elevation: 0,
-                                  backgroundColor:
-                                      Theme.of(context).primaryColor,
-                                  foregroundColor:
-                                      Theme.of(context).canvasColor,
-                                  child: Icon(
-                                    Icons.add,
-                                    size: Theme.of(context)
-                                        .textTheme
-                                        .headline1
-                                        .fontSize,
-                                  ),
-                                  onPressed: null,
-                                ),
-                              ),
-                            ),
-                            Padding(
-                              padding: const EdgeInsets.all(16.0),
-                              child: Align(
-                                alignment: Alignment.bottomCenter,
-                                child: Text(
-                                  'Quittle',
-                                  style: TextStyle(
-                                    fontSize: Theme.of(context)
-                                        .textTheme
-                                        .headline2
-                                        .fontSize,
-                                    color: Theme.of(context).primaryColor,
-                                    fontWeight: FontWeight.bold,
-                                  ),
-                                ),
-                              ),
-                            )
-                          ],
+    return Scaffold(
+      key: _scaffoldKey,
+      appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(Icons.menu),
+          onPressed: () {
+            _scaffoldKey.currentState.openDrawer();
+          },
+        ),
+      ),
+      floatingActionButton: Consumer<AddictionsProvider>(
+        child: FloatingActionButton(
+          heroTag: 'newAddiction',
+          onPressed: pushCreateAddictionScreen,
+          backgroundColor: Theme.of(context).primaryColor,
+          tooltip: 'New',
+          child: Icon(Icons.add),
+        ),
+        builder: (_, addictionsData, child) {
+          return addictionsData.addictions.length == 0
+              ? SizedBox()
+              : SizedBox(
+                  child: child,
+                );
+        },
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      // bottomNavigationBar: Builder(
+      //   builder: (context) => BottomAppBar(
+      //     child: Row(
+      //       mainAxisAlignment: MainAxisAlignment.start,
+      //       children: [
+      //         IconButton(
+      //           onPressed: () {
+      //             Scaffold.of(context).openDrawer();
+      //           },
+      //           color: Theme.of(context).primaryColor,
+      //           tooltip: 'Menu',
+      //           icon: Icon(Icons.menu),
+      //         ),
+      //       ],
+      //     ),
+      //   ),
+      // ),
+      drawer: SettingsView(),
+      body: Consumer<AddictionsProvider>(
+        builder: (ctx, addictionsData, child) {
+          _setProgNotifTasks(addictionsData.addictions);
+          return addictionsData.addictions.length > 0
+              ? ReorderableListView(
+                  children: [
+                    for (Addiction addiction in addictionsData.addictions)
+                      Dismissible(
+                        key: ValueKey(Uuid().v1()),
+                        child: AddictionItem(
+                          addictionData: addiction,
                         ),
                       ),
-                    );
-            },
-          ),
-        ),
+                  ],
+                  onReorder: (oldIndex, newIndex) {
+                    setState(() {
+                      if (newIndex > oldIndex) {
+                        newIndex--;
+                      }
+                      Provider.of<AddictionsProvider>(context, listen: false)
+                          .reorderAddictions(oldIndex, newIndex);
+                    });
+                  },
+                )
+              //  ListView.builder(
+              //     itemCount: addictionsData.addictions.length,
+              //     itemBuilder: (ctx, index) {
+              //       return AddictionItem(
+              //         addictionData: addictionsData.addictions[index],
+              //       );
+              //     },
+              //   )
+              : InkWell(
+                  onTap: pushCreateAddictionScreen,
+                  child: Container(
+                    height: deviceSize.height,
+                    width: deviceSize.width,
+                    child: Stack(
+                      alignment: Alignment.center,
+                      children: [
+                        Center(
+                          child: SizedBox.fromSize(
+                            size: Size.square(deviceSize.width * .5),
+                            child: FloatingActionButton(
+                              heroTag: 'newAddiction',
+                              elevation: 0,
+                              backgroundColor: Theme.of(context).primaryColor,
+                              foregroundColor: Theme.of(context).canvasColor,
+                              child: Icon(
+                                Icons.add,
+                                size: Theme.of(context)
+                                    .textTheme
+                                    .headline1
+                                    .fontSize,
+                              ),
+                              onPressed: null,
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(16.0),
+                          child: Align(
+                            alignment: Alignment.bottomCenter,
+                            child: Text(
+                              'Quittle',
+                              style: TextStyle(
+                                fontSize: Theme.of(context)
+                                    .textTheme
+                                    .headline2
+                                    .fontSize,
+                                color: Theme.of(context).primaryColor,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                );
+        },
       ),
     );
   }
